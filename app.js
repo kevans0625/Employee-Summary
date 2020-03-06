0940e35b3df7167f19c7ef250eb2df2693c93f0f
Kristin Evans <kevans0625@gmail.com>
2020-03-06 01:33:02 -0600

    Updated templates.

---
+++ app.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+
+// This constructor is responsible for getting the user info and controlling the flow of the app
+
+class app {
+    // prompt the questions to gather information about how many people are on the teams, each employee
+    // their email, id, and specific role based on
+    //  their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
+    // 
+}
