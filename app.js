1c92534cf1e8658b9f8ded6a6bf153c4c26dc6fc
Kristin Evans <kevans0625@gmail.com>
2020-03-06 02:07:28 -0600

    created user prompt

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const inquirer = require("inquirer");
 
-// This constructor is responsible for getting the user info and controlling the flow of the app
+//Gather information on the user...what role are they
 
-class app {
+function promptUser() {
     // prompt the questions to gather information about how many people are on the teams, each employee
     // their email, id, and specific role based on
     //  their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
     // 
-}
+    // * Name
+
+    //     * Role
+
+    //     * ID
+
+    //     * Role - specific property
+
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Welcome. Please select your role on this team.",
+            name: "username",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager",
+                "Other employees"
+            ]
+
+        
+        }   //based on their response switch between functions to grab the proper employee type
+    ])
+       console.log(response);
+        };
+        promptUser()
+
+
